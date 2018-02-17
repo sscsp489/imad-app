8d15eaaf12383bb77148293f42f549235a4008e9
sscsp489 <36526662+sscsp489@users.noreply.github.com>
2018-02-17 10:51:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one.html',function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/article1',function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-app.get('/article-second.html',function (req,res){
+app.get('/article2',function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-second.html'));  
 });
-app.get('/article-third.html',function(req,res){
+app.get('/article3',function(req, res) {
      res.sendFile(path.join(__dirname, 'ui', 'article-third.html'));
 });
 
