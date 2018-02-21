919a3d5a024b7eba8853c3d17fe2dd9e890f0568
sscsp489 <36526662+sscsp489@users.noreply.github.com>
2018-02-21 10:58:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,23 @@
 var button = document.getElementById('counterr');
-var counter= 0;
+
 
 button.onclick = function () {
+    var request = new XMLHttprequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttprequest.DONE){
+            if(request.status === 200 ) {
+                var counter = request.responseText;
+                var span = document.getElementById('countt');
+                span.innerHTML = counter.toString();
+                
+            }
+        }
+        
+    }
     
+request.open('GET','http://sscsp489.imad.hasura-app.io/counter',true);
+request.send(null);
+
     
-    
-    
-    counter = counter + 1 ;
-    var span = document.getElementById('countt');
-    span.innerHTML = counter.toString();
+   
 };
