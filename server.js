c97f3f7d6cb6c50b71e877fd1affb05462b7d8e6
sscsp489 <36526662+sscsp489@users.noreply.github.com>
2018-02-21 09:55:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -40,7 +40,10 @@ return htmltemplate;
 }
 
 
-
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter,tostring());
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
