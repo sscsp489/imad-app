59394f3d53b20c502125e4748fd6c5a9b897e104
sscsp489 <36526662+sscsp489@users.noreply.github.com>
2018-03-03 12:30:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pd').pool;
+var config = {
+    user : 'sscsp489' ,
+    database : 'sscsp489',
+    host : 'db.imad.hasura-app.io',
+    port : '5432'
+    password : process.env.DB_PASSWORD 
+};
+
 
 var app = express();
 app.use(morgan('combined'));
@@ -38,8 +47,20 @@ app.get('/counter',function(req,res){
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  
 });
+var pool = new pool(config);
 
+app.get('/test-db' , function (req,res) {
+    pool.query('SELECT * FROM test',function(err,result){
+        if(err){
+            res.status(500).send(err, toString());
+            
+        }else {
+            res.send(JSON.stringify(result));
+        }
+    });
+});
 app.get('/article1',function (req, res) {
   res.send(createtemplate(article1));
 });
